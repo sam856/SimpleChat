f129ddfc49ced26c8e704f61067be7cfbb68bfe4
Menna <mennanary@gmail.com>
2024-06-26 00:14:39 +0300

    first commit

---
+++ Chat/wwwroot/js/chat.js
@@ -0,0 +1,19 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .build();
+
+connection.on("ReceiveMessage", (user, message) => {
+    const encodedUser = user ? `${user}: ` : '';
+    const li = document.createElement("li");
+    li.textContent = encodedUser + message;
+    document.getElementById("chatMessages").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", async (event) => {
+    const username = document.getElementById("usernameInput").value;
+    const message = document.getElementById("messageInput").value;
+    await connection.invoke("SendMessage", username, message);
+    document.getElementById("messageInput").value = '';
+});
+
+connection.start().catch(err => console.error(err.toString()));
